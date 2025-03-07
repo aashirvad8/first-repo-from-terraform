d501ff09a681a06a75eddca52716da688f048ee2
aashirvad8 <aashirvad8@gmail.com>
2025-03-07 12:31:33 +0530

    Add files via upload

---
+++ network.tf
@@ -0,0 +1,34 @@
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 2.3"
+
+    project_id   = var.project_name
+    network_name = var.custom_network
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = "us-west1"
+        }
+    ]
+
+    routes = [
+        {
+            name                   = "egress-internet"
+            description            = "route through IGW to access internet"
+            destination_range      = "0.0.0.0/0"
+            tags                   = "egress-inet"
+            next_hop_internet      = "true"
+        },
+        {
+            name                   = "app-proxy"
+            description            = "route through proxy to reach app"
+            destination_range      = "10.50.10.0/24"
+            tags                   = "app-proxy"
+            next_hop_instance      = "app-proxy-instance"
+            next_hop_instance_zone = "us-west1-a"
+        },
+    ]
+}
